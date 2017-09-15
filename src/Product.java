216b47e730fbb9d19290ba38745944a6d83adf02
Kuba Mrozowski <stasmrozowski@gmail.com>
2017-09-15 11:52:05 +0200

    zadanie 2A

---
+++ src/Product.java
@@ -0,0 +1,14 @@
+class Product {
+    String name;
+    String producer;
+    String sku;
+    double price;
+
+    Product(String n, String p, String s, double pr){
+        name = n;
+        producer = p;
+        sku = s;
+        price =pr;
+
+    }
+}
